3bab9585f89bcae7404158c450e46c168b55b048
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-10-18 23:23:21 +0900

    default_ui_update

---
+++ DrinkWater/Alert.swift
@@ -0,0 +1,27 @@
+//
+//  Alert.swift
+//  DrinkWater
+//
+//  Created by 양성혜 on 2022/10/18.
+//
+
+import Foundation
+
+struct Alert: Codable{
+    var id: String = UUID().uuidString
+    let date: Date
+    let isOn: Bool
+    
+    var time: String {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "hh:mm"
+        return timeFormatter.string(from: date)
+    }
+    
+    var meridiem: String {
+        let meridiemFormatter = DateFormatter()
+        meridiemFormatter.dateFormat = "a"
+        meridiemFormatter.locale = Locale(identifier: "ko")
+        return meridiemFormatter.string(from: date)
+    }
+}
--- DrinkWater/AlertListViewController.swift
+++ DrinkWater/AlertListViewController.swift
@@ -9,6 +9,58 @@ import UIKit
 
 class AlertListViewController: UITableViewController {
     
+    var alertList : [Alert] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let nibName = UINib(nibName: "AlertListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "AlertListCell")
+    }
     @IBAction func addAlertButtonAction(_ sender: UIBarButtonItem) {
+    }
+}
+
+//UITableView Datasource,Delegate
+extension AlertListViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return alertList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "물 마실 시간"
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AlertListCell",for: indexPath) as? AlertListCell else {return UITableViewCell()}
+        
+        cell.alertSwitch.isOn = alertList[indexPath.row].isOn
+        cell.timeLabel.text = alertList[indexPath.row].time
+        cell.meriditemLabel.text = alertList[indexPath.row].meridiem
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            return
+        default:
+            break
+        }
     }
 }
